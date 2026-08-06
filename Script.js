2c09c5490506a1aab99310c35b9871f970888906
kartik572 <154071941+kartik572@users.noreply.github.com>
2026-08-06 05:35:58 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("startBtn");
+
+button.addEventListener("click", () => {
+
+    button.innerHTML = "❤️ I Love You Bachaaaaa ❤️";
+
+    document.body.style.background =
+        "linear-gradient(135deg,#ff4f93,#8a2be2,#111)";
+
+    alert(
+`Happy Birthday, My Bachaaaaa ❤️
+
+This is only the beginning...
+
+I made this entire website just for you.
+
+- Yours ❤️`
+);
+
+});
